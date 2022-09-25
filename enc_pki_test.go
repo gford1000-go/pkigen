2e532b09896ef1764c7a592746cc925cacc73a56
Geoff Ford <gford1000@hotmail.com>
2022-09-25 17:14:01 +0100

    Added more tests

--- enc_pki_test.go
+++ enc_pki_test.go
@@ -29,3 +29,26 @@ func TestEnc(t *testing.T) {
 	// Demonstrate that the unencrypted data is valid
 	verifyContentsOfBase64EncodedRSAKey(b, t)
 }
+
+func TestEncPubKey(t *testing.T) {
+
+	// Generate my own personal one time key pair
+	privKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Ask for data encrypted with my one time key pair
+	e, err := CreateEncryptedRSAKey(&privKey.PublicKey, 2048)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	pubKey, err := e.UnmarshalEnvelopePublicKey()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Demonstrate that the returned public key works
+	verifyRSAKeyPair(pubKey, privKey, t)
+}
--- pki_test.go
+++ pki_test.go
@@ -8,16 +8,27 @@ import (
 	"testing"
 )
 
-func verifyContentsOfBase64EncodedRSAKey(b *Base64EncodedRSAKey, t *testing.T) {
-	pubKey, err := UnmarshalPublicKey(b)
+func verifyRSAKeyPair(pub *rsa.PublicKey, priv *rsa.PrivateKey, t *testing.T) {
+	secretMessage := []byte("my secret message")
+	label := []byte("my label")
+
+	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pub, secretMessage, label)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	secretMessage := []byte("my secret message")
-	label := []byte("my label")
+	plaintext, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, priv, ciphertext, label)
+	if err != nil {
+		t.Fatal(err)
+	}
 
-	ciphertext, err := rsa.EncryptOAEP(sha256.New(), rand.Reader, pubKey, secretMessage, label)
+	if !bytes.Equal(secretMessage, plaintext) {
+		t.Errorf("Mismatch: expected '%s', received '%s'", secretMessage, plaintext)
+	}
+}
+
+func verifyContentsOfBase64EncodedRSAKey(b *Base64EncodedRSAKey, t *testing.T) {
+	pubKey, err := UnmarshalPublicKey(b)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -27,14 +38,7 @@ func verifyContentsOfBase64EncodedRSAKey(b *Base64EncodedRSAKey, t *testing.T) {
 		t.Fatal(err)
 	}
 
-	plaintext, err := rsa.DecryptOAEP(sha256.New(), rand.Reader, privKey, ciphertext, label)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	if !bytes.Equal(secretMessage, plaintext) {
-		t.Errorf("Mismatch: expected '%s', received '%s'", secretMessage, plaintext)
-	}
+	verifyRSAKeyPair(pubKey, privKey, t)
 }
 
 func TestGen(t *testing.T) {
